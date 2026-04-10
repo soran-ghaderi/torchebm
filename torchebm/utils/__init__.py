02bbb2a2c4ae4a0a15b87a60b80134766960a9c2
Soran Ghaderi <soran.gdr.cs@gmail.com>
2026-04-10 20:53:46 +0100

    perf: Implement lazy loading for subpackages and remove unused image and visualization utilities

--- torchebm/utils/__init__.py
+++ torchebm/utils/__init__.py
@@ -1,12 +1,5 @@
 r"""Utility functions for TorchEBM."""
 
-from .visualization import (
-    plot_2d_energy_landscape,
-    plot_3d_energy_landscape,
-    plot_samples_on_energy,
-    plot_sample_trajectories,
-)
-
 from .training import (
     update_ema,
     requires_grad,
@@ -14,23 +7,9 @@ from .training import (
     load_checkpoint,
 )
 
-from .image import (
-    center_crop_arr,
-    create_npz_from_sample_folder,
-)
-
 __all__ = [
-    # Visualization
-    "plot_2d_energy_landscape",
-    "plot_3d_energy_landscape",
-    "plot_samples_on_energy",
-    "plot_sample_trajectories",
-    # Training
     "update_ema",
     "requires_grad",
     "save_checkpoint",
     "load_checkpoint",
-    # Image
-    "center_crop_arr",
-    "create_npz_from_sample_folder",
 ]
