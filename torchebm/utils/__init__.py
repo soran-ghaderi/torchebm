942f3eb2147a8634c3c5edcbde1aa80ffc10e94f
Soran Ghaderi <soran.gdr.cs@gmail.com>
2026-04-10 20:53:46 +0100

    perf: Implement lazy loading for subpackages and remove unused image and visualization utilities

--- torchebm/utils/__init__.py
+++ torchebm/utils/__init__.py
@@ -6,12 +6,18 @@ from .training import (
     save_checkpoint,
     load_checkpoint,
 )
+<<<<<<< HEAD
 from .profiling import profile_context
+=======
+>>>>>>> ec7b9371 (perf: Implement lazy loading for subpackages and remove unused image and visualization utilities)
 
 __all__ = [
     "update_ema",
     "requires_grad",
     "save_checkpoint",
     "load_checkpoint",
+<<<<<<< HEAD
     "profile_context",
+=======
+>>>>>>> ec7b9371 (perf: Implement lazy loading for subpackages and remove unused image and visualization utilities)
 ]
