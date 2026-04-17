a23e43c48d9af81956cd2cf3f549c61a7426f0d2
Soran Ghaderi <soran.gdr.cs@gmail.com>
2026-04-17 23:44:47 +0100

    docs: add profiling and benchmarking sections to documentation and update .gitignore

--- torchebm/utils/__init__.py
+++ torchebm/utils/__init__.py
@@ -6,10 +6,12 @@ from .training import (
     save_checkpoint,
     load_checkpoint,
 )
+from .profiling import profile_context
 
 __all__ = [
     "update_ema",
     "requires_grad",
     "save_checkpoint",
     "load_checkpoint",
+    "profile_context",
 ]
